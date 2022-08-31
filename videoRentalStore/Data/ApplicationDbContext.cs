f5de590194c63df5a7dee95d0682a69a76f9c9dd
KuArthur <persjke@gmail.com>
2022-08-31 14:32:38 +0300

    feat(Models): added Video model

--- videoRentalStore/Data/ApplicationDbContext.cs
+++ videoRentalStore/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using videoRentalStore.Models;
 
 namespace videoRentalStore.Data;
 
 public class ApplicationDbContext : IdentityDbContext
 {
+    public DbSet<Video> Videos { get; set; }
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options)
     {
